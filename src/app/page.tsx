0c641c278b06200ca29535285324c36f4f87458d
KaySum <43190281+KaySum@users.noreply.github.com>
2025-08-14 16:18:08 -0400

    feat: Initialize HTML input with example HTML and added "clear" button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { domToPng } from "modern-screenshot";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CodeEditor from "@/components/CodeEditor";
 import { Button } from "@/components/ui/button";
+import { exampleHtml } from "@/data/example";
 import { author, authorUrl, description, title } from "@/data/info";
 
 export default function Home() {
   const [htmlInput, setHtmlInput] = useState("");
+
+  useEffect(() => {
+    setHtmlInput(exampleHtml);
+  }, []);
 
   const saveScreenshot = () => {
     domToPng(document.querySelector("#screenshot-element") as Node).then(
@@ -22,7 +27,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="m-5 flex justify-between">
+      <div className="m-5 flex justify-between gap-5">
         <div className="flex flex-col gap-2">
           <h1 className="font-bold text-2xl">{title}</h1>
           <h2 className="text-xl">
@@ -33,12 +38,23 @@ export default function Home() {
           </h2>
           <p>{description}</p>
         </div>
-
-        <Button onClick={saveScreenshot} type="button">
-          Download
-        </Button>
+        <div className="flex flex-wrap gap-2">
+          <Button
+            onClick={() => setHtmlInput("")}
+            type="button"
+            variant="destructive"
+          >
+            Clear
+          </Button>
+          <Button onClick={saveScreenshot} type="button">
+            Download
+          </Button>
+        </div>
       </div>
-      <CodeEditor onChange={(value) => setHtmlInput(value ?? "")} />
+      <CodeEditor
+        onChange={(value) => setHtmlInput(value ?? "")}
+        value={htmlInput}
+      />
       <div className="m-5">
         <div
           className="size-fit"
---
+++ src/data/example.ts
@@ -0,0 +1,14 @@
+export const exampleHtml = `<!-- Both TailwindCSS and regular CSS via style tag work! -->
+<style>
+    .container {
+        border: solid 0.25rem lime;
+        padding: 1rem;
+        background-color: orange;
+        border-radius: 1rem;
+    }
+</style>
+
+<div class="container">
+    <div class="size-32 rounded-full bg-conic from-blue-500 via-blue-900 to-blue-500" />
+</div>
+`;
